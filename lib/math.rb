93b1efbefb9e1eed8de648f12d95da3a773c400b
Jonathan Koss <jonathan_koss@yahoo.com>
2019-08-23 01:15:22 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
+  added_nums = num1 + num2
   
+  added_nums
 end
 
 def subtraction(num1, num2)
-  
+  num1 - num2
 end
 
 def division(num1, num2)
-  
+  num1/num2
 end
 
 def multiplication(num1, num2)
-  
+  num1 * num2
 end
 
 def modulo(num1, num2)
-  
+  num1 % num2
 end
 
 def square_root(num)
-  
+  Integer.sqrt(num)
 end
